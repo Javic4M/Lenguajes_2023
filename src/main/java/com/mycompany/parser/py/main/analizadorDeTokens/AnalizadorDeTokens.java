129cf569ee7f2379b96a264dab6ba12a5157a127
Javier_CM <javiercoyoy202030556@cunoc.edu.gt>
2023-10-13 23:06:32 -0600

    Practica 2 LF

--- src/main/java/com/mycompany/parser/py/main/analizadorDeTokens/AnalizadorDeTokens.java
+++ src/main/java/com/mycompany/parser/py/main/analizadorDeTokens/AnalizadorDeTokens.java
@@ -137,20 +137,22 @@ public class AnalizadorDeTokens {
                                     }
                                 }
                             }
-//                            while (!estado) {
-//                                if ((tokensIdentificados.obtenerContenido(indice).obtenerTipoDeToken().equals("Identificador") || tokensIdentificados.obtenerContenido(indice).obtenerTipoDeToken().equals("Constante")) && estaEnLaMismaLinea()) {
-//                                    variables.agregarALaLista(indice);
-//                                    indice++; estructuraCompletada = true;
-//                                    
-//                                    if (tokensIdentificados.obtenerContenido(indice).obtenerTipoDeToken().equals("Aritmetico") && estaEnLaMismaLinea()) {
-//                                        indice++; estructuraCompletada = false;               
-//                                    } else {
-//                                        estado = analizarOperacionAsignacion(variables, indiceIdentificador);
-//                                    }
-//                                } else {
-//                                    estado = salir = imprimiError(", se esperaba un Identificador ó una Constante, recuerde no sumar Cadenas");
-//                                }
-//                            }
+                            /*
+                            while (!estado) {
+                                if ((tokensIdentificados.obtenerContenido(indice).obtenerTipoDeToken().equals("Identificador") || tokensIdentificados.obtenerContenido(indice).obtenerTipoDeToken().equals("Constante")) && estaEnLaMismaLinea()) {
+                                    variables.agregarALaLista(indice);
+                                    indice++; estructuraCompletada = true;
+                                    
+                                    if (tokensIdentificados.obtenerContenido(indice).obtenerTipoDeToken().equals("Aritmetico") && estaEnLaMismaLinea()) {
+                                        indice++; estructuraCompletada = false;               
+                                    } else {
+                                        estado = analizarOperacionAsignacion(variables, indiceIdentificador);
+                                    }
+                                } else {
+                                    estado = salir = imprimiError(", se esperaba un Identificador ó una Constante, recuerde no sumar Cadenas");
+                                }
+                            }
+                            */
                         } else if (tokensIdentificados.obtenerContenido(indice).obtenerTipoDeToken().equals("Comparacion") && estaEnLaMismaLinea()) {
                             indice++;
                             boolean segundoIdentificador = true;
